57ed04e7f5164542642bcceff349c6fa8290927e
SHANG-TING, SIE <rebom8888@gmail.com>
2018-11-07 00:02:19 +0800

    ng g c card-or-list-view

---
+++ src/app/card-or-list-view/card-or-list-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-card-or-list-view',
+  templateUrl: './card-or-list-view.component.html',
+  styleUrls: ['./card-or-list-view.component.scss']
+})
+export class CardOrListViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
