aacc47b1ed73b9b58238963cff62cf5da1d10298
SHANG-TING, SIE <rebom8888@gmail.com>
2018-11-07 00:19:43 +0800

    NgTemplateOutlet is a directive that takes a TemplateRef and context and stamps out an EmbeddedViewRef with the provided context. The context is accessed on the template via let-{{templateVariableName}}=”contextProperty” attributes to create a variable the template can use. If a context property name is not provided, it will choose the $implicit property. The following example:

--- src/app/ng-template-outlet-example/ng-template-outlet-example.component.ts
+++ src/app/ng-template-outlet-example/ng-template-outlet-example.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-ng-template-outlet-example',
-  templateUrl: './ng-template-outlet-example.component.html',
-  styleUrls: ['./ng-template-outlet-example.component.scss']
+  selector: 'ng-template-outlet-example',
+  template: `
+    <ng-container *ngTemplateOutlet="templateRef; context: exampleContext"></ng-container>
+    <ng-template #templateRef let-default let-other="aContextProperty">
+      <div>
+        $implicit = '{{default}}'
+        aContextProperty = '{{other}}'
+      </div>
+    </ng-template>
+`,
 })
-export class NgTemplateOutletExampleComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class NgTemplateOutletExampleComponent {
+  exampleContext = {
+    $implicit: 'default context property when none specified',
+    aContextProperty: 'a context property',
+  };
 }
