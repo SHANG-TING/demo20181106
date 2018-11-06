33482a72f49a65dd1ab4a684cd10f88d74e3b49b
SHANG-TING, SIE <rebom8888@gmail.com>
2018-11-07 00:03:01 +0800

    ng g c usage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CardOrListViewComponent } from './card-or-list-view/card-or-list-view.component';
+import { UsageComponent } from './usage/usage.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CardOrListViewComponent
+    CardOrListViewComponent,
+    UsageComponent
   ],
   imports: [
     BrowserModule,
